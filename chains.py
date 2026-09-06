48a75ecf35dc9d181fcbcdaf5c605345a1e0f467
VaibhavYK <vaibhavkulkarni270@gmail.com>
2026-09-07 01:40:07 +0530

    Switch Groq model to llama-3.1-8b-instant

--- chains.py
+++ chains.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
@@ -18,7 +19,7 @@ class Chain:
         self.llm = ChatGroq(
             temperature=0,
             groq_api_key=api_key,
-            model_name="llama-3.3-70b-versatile",
+            model_name="llama-3.1-8b-instant",
         )
 
     def extract_jobs(self, cleaned_text):
